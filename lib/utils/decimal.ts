78325685905766298c9b6ba7fb7c20245de2c28a
NightCatSama <642163903@qq.com>
2018-11-14 18:33:12 +0800

    wip: add prettier

--- lib/utils/decimal.ts
+++ lib/utils/decimal.ts
@@ -1,93 +1,94 @@
-type Operator = '+' | '-' | '*' | '/' | '%'
+type Operator = '+' | '-' | '*' | '/' | '%';
 
 export default class Decimal {
-  num: number
+  num: number;
 
   constructor(num: number) {
-    this.num = num
+    this.num = num;
   }
 
-  decimal(num2: number, operator: Operator, isChain?: false): number
-  decimal(num2: number, operator: Operator, isChain: true): this
+  decimal(num2: number, operator: Operator, isChain?: false): number;
+  decimal(num2: number, operator: Operator, isChain: true): this;
   decimal(num2: number, operator: Operator, isChain?: boolean): number | this {
-    const num1 = this.num
-    const decimals1 = `${num1}`.split('.')[1] || ''
-    const decimals2 = `${num2}`.split('.')[1] || ''
-    const decimals = decimals1.length > decimals2.length ? decimals1 : decimals2
-    let multiple = decimals ? Math.pow(10, decimals.length) : 1
-    const n1 = Math.floor(num1 * multiple)
-    const n2 = Math.floor(num2 * multiple)
-    let n = 0
+    const num1 = this.num;
+    const decimals1 = `${num1}`.split('.')[1] || '';
+    const decimals2 = `${num2}`.split('.')[1] || '';
+    const decimals =
+      decimals1.length > decimals2.length ? decimals1 : decimals2;
+    let multiple = decimals ? Math.pow(10, decimals.length) : 1;
+    const n1 = Math.floor(num1 * multiple);
+    const n2 = Math.floor(num2 * multiple);
+    let n = 0;
     switch (operator) {
       case '+':
-        n = n1 + n2
-        break
+        n = n1 + n2;
+        break;
       case '-':
-        n = n1 - n2
-        break
+        n = n1 - n2;
+        break;
       case '*':
-        n = n1 * n2
-        multiple *= multiple
-        break
+        n = n1 * n2;
+        multiple *= multiple;
+        break;
       case '/':
-        n = n1 / n2
-        multiple = 1
-        break
+        n = n1 / n2;
+        multiple = 1;
+        break;
       case '%':
-        n = n1 % n2
-        multiple = 1
-        break
+        n = n1 % n2;
+        multiple = 1;
+        break;
     }
 
-    this.num = n / multiple
+    this.num = n / multiple;
     if (isChain) {
-      return this
+      return this;
     } else {
-      return this.num
+      return this.num;
     }
   }
 
   plus(num2: number) {
-    return this.decimal(num2, '+', false)
+    return this.decimal(num2, '+', false);
   }
 
   plusChain(num2: number) {
-    return this.decimal(num2, '+', true)
+    return this.decimal(num2, '+', true);
   }
 
   minus(num2: number) {
-    return this.decimal(num2, '-', false)
+    return this.decimal(num2, '-', false);
   }
 
   minusChain(num2: number) {
-    return this.decimal(num2, '-', true)
+    return this.decimal(num2, '-', true);
   }
 
   multiply(num2: number) {
-    return this.decimal(num2, '*', false)
+    return this.decimal(num2, '*', false);
   }
 
   multiplyChain(num2: number) {
-    return this.decimal(num2, '*', true)
+    return this.decimal(num2, '*', true);
   }
 
   divide(num2: number) {
-    return this.decimal(num2, '/', false)
+    return this.decimal(num2, '/', false);
   }
 
   divideChain(num2: number) {
-    return this.decimal(num2, '/', true)
+    return this.decimal(num2, '/', true);
   }
 
   remainder(num2: number) {
-    return this.decimal(num2, '%', false)
+    return this.decimal(num2, '%', false);
   }
 
   remainderChain(num2: number) {
-    return this.decimal(num2, '%', true)
+    return this.decimal(num2, '%', true);
   }
 
   toNumber(): number {
-    return this.num
+    return this.num;
   }
 }
--- lib/utils/state.ts
+++ lib/utils/state.ts
@@ -1,36 +1,36 @@
 export interface StateMap {
-  [key: string]: number
+  [key: string]: number;
 }
 
 export default class State {
-  map: StateMap
-  states: number = 0
+  map: StateMap;
+  states: number = 0;
 
   constructor(map: StateMap) {
-    this.map = map
+    this.map = map;
   }
 
   // 设置状态
   add(state: number) {
-    this.states |= state
+    this.states |= state;
   }
 
   // 移除状态
   delete(state: number) {
-    this.states &= ~state
+    this.states &= ~state;
   }
 
   // 切换状态
   toggle(state: number) {
     if (this.has(state)) {
-      this.delete(state)
+      this.delete(state);
     } else {
-      this.add(state)
+      this.add(state);
     }
   }
 
   // 判断是否存在该状态
   has(state: number): boolean {
-    return !!(this.states & state)
+    return !!(this.states & state);
   }
 }
